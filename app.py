05f6dfbd24011f3235af402b8ac1b541fb147b4c
Dumitru Cambur <dcambur@gmail.com>
2021-10-14 09:44:28 +0300

    more cleanup

--- app.py
+++
@@ -1,6 +0,0 @@
-from dining_hall_api import create_app
-from configuration.flask_config import DevelopmentConfig
-
-if __name__ == "__main__":
-    app = create_app(config_object=DevelopmentConfig())
-    app.run(host='0.0.0.0', port="5001")
